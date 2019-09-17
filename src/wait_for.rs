e32831a4c848e46d789b8b6a663c0913276b93e8
Jon Foss Mikalsen <jon@orcalabs.no>
2019-09-17 12:38:54 +0200

    feat: remove DefaultWait and implement RunningWait and ExitWait
    RunningWait waits for the container to appear as running.
    ExitWait waits for the container to exit.
    
    The ExitWait implementation needs to be altered as it now only checks
    that the running status is false (which is probably not enough to be
    sure the container started and exited).

--- src/wait_for.rs
+++ src/wait_for.rs
@@ -1,6 +1,10 @@
 use crate::container::Container;
-use failure::Error;
+use failure::{format_err, Error};
 use futures::future::{self, Future};
+use futures::stream::Stream;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+use tokio::timer::Interval;
 
 /// Trait to wait for a container to be ready for service.
 pub trait WaitFor {
@@ -14,18 +18,124 @@ pub trait WaitFor {
     ) -> Box<dyn Future<Item = Container, Error = Error>>;
 }
 
-/// The default wait implementation for containers.
-/// This implementation should wait for containers to appear
-/// as running.
-pub struct DefaultWait {}
+/// The RunningWait implementation for containers.
+/// This implementation will wait untill the docker daemon reports the container as running
+pub struct RunningWait {
+    pub check_interval: i32,
+    pub max_checks: i32,
+}
 
-impl WaitFor for DefaultWait {
+/// The ExitedWait implementation for containers.
+/// This implementation will wait untill the docker daemon reports that the container has exited
+pub struct ExitedWait {
+    pub check_interval: i32,
+    pub max_checks: i32,
+}
+
+/// The NoWait implementation for containers.
+/// This implementation does not wait for anything, resolves immediatley.
+pub struct NoWait {}
+
+impl WaitFor for RunningWait {
+    fn wait_for_ready(
+        &self,
+        container: Container,
+    ) -> Box<dyn Future<Item = Container, Error = Error>> {
+        let liveness_future =
+            wait_for_container_state(container, self.check_interval, self.max_checks, |state| {
+                state.running
+            });
+
+        Box::new(liveness_future)
+    }
+}
+
+impl WaitFor for ExitedWait {
+    fn wait_for_ready(
+        &self,
+        container: Container,
+    ) -> Box<dyn Future<Item = Container, Error = Error>> {
+        let liveness_future =
+            wait_for_container_state(container, self.check_interval, self.max_checks, |state| {
+                !state.running
+            });
+
+        Box::new(liveness_future)
+    }
+}
+
+impl WaitFor for NoWait {
+>>>>>>> 30d6eaf... tmp
     fn wait_for_ready(
         &self,
         container: Container,
     ) -> Box<dyn Future<Item = Container, Error = Error>> {
         Box::new(future::ok(container))
     }
+}
+
+fn wait_for_container_state(
+    container: Container,
+    check_interval: i32,
+    max_checks: i32,
+    container_state_compare: fn(&shiplift::rep::State) -> bool,
+) -> impl Future<Item = Container, Error = Error> {
+    let client = shiplift::Docker::new();
+    let container_name = container.name().to_string();
+
+    let desired_state = Arc::new(Mutex::new(false));
+
+    let desired_state_clone = desired_state.clone();
+    let desired_state_clone2 = desired_state.clone();
+
+    Interval::new(Instant::now(), Duration::from_millis(check_interval as u64))
+        // Limits us to only check status for the given amount of tries
+        .take(max_checks as u64)
+        .map_err(|e| format_err!("failed to check container liveness: {}", e))
+        // While continue checking container status untill the desired state is reached.
+        // If the desired state is reached we return false to stop the stream.
+        .take_while(move |_| {
+            let s = desired_state_clone.lock().unwrap();
+            if *s {
+                future::ok(false)
+            } else {
+                future::ok(true)
+            }
+        })
+        .for_each(move |_| {
+            let desired_state_clone3 = desired_state.clone();
+            client
+                .containers()
+                .get(&container_name)
+                .inspect()
+                .map_err(|e| format_err!("failed to inspect container: {}", e))
+                .and_then(move |c| {
+                    if container_state_compare(&c.state) {
+                        let mut s = desired_state_clone3.lock().unwrap();
+                        *s = true;
+                    }
+
+                    future::ok(())
+                })
+        })
+        .then(move |r| {
+            // We failed checking the status of the container
+            if r.is_err() {
+                future::Either::B(future::err(format_err!("{}", r.unwrap_err())))
+            } else {
+                let s = desired_state_clone2.lock().unwrap();
+                // The desired status has been reached and we can return Ok
+                if *s {
+                    future::Either::A(future::ok(container))
+                } else {
+                    // The desired status was not reached and we return an error
+                    future::Either::B(future::err(format_err!(
+                        "container failed to reach desired container state, container name: {}",
+                        container.name()
+                    )))
+                }
+            }
+        })
 }
 
 #[cfg(test)]
