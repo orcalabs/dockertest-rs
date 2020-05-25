ba8cd6546a0fa15f5d672eb1a660235dce50e7fa
Vegard Sandengen <vegard@orcalabs.no>
2020-05-25 14:05:24 +0200

    test: add multiple_containers test for inject_container_name_env

---
+++ tests/multiple_containers.rs
@@ -0,0 +1,34 @@
+use dockertest::waitfor::{MessageSource, MessageWait};
+use dockertest::{Composition, DockerTest, StartPolicy};
+use test_env_log::test;
+
+#[test]
+fn test_inject_container_name_ip_through_env_communication() {
+    let mut test = DockerTest::new();
+
+    let recv = Composition::with_repository("dockertest-rs/coop_recv")
+        .with_start_policy(StartPolicy::Strict)
+        .with_wait_for(Box::new(MessageWait {
+            message: "recv started".to_string(),
+            source: MessageSource::Stdout,
+            timeout: 10,
+        }))
+        .with_container_name("recv");
+    test.add_composition(recv);
+
+    let mut send = Composition::with_repository("dockertest-rs/coop_send")
+        .with_start_policy(StartPolicy::Strict)
+        .with_wait_for(Box::new(MessageWait {
+            message: "send success".to_string(),
+            source: MessageSource::Stdout,
+            timeout: 60,
+        }));
+    send.inject_container_name("recv", "SEND_TO_IP");
+    test.add_composition(send);
+
+    test.run(|_ops| async move {
+        // This test is executed through the WaitFor mechanism
+        // TODO: assert in the log output for recv that it actually received the data.
+        assert!(true);
+    });
+}
