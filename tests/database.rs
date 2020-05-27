1a807cd33dada6299ac64ff1b7b1ee80fdd57669
Vegard Sandengen <vegard@orcalabs.no>
2020-05-27 11:03:52 +0200

    test: fix database test after updated postgres image

--- tests/database.rs
+++ tests/database.rs
@@ -10,17 +10,18 @@ fn test_connect_to_postgres_through_container_ip() {
     let mut test = DockerTest::new().with_default_source(source);
 
     let repo = "postgres";
-    let postgres = Composition::with_repository(repo).with_wait_for(Box::new(MessageWait {
+    let mut postgres = Composition::with_repository(repo).with_wait_for(Box::new(MessageWait {
         message: "database system is ready to accept connections".to_string(),
         source: MessageSource::Stderr,
         timeout: 20,
     }));
+    postgres.env("POSTGRES_PASSWORD", "password");
 
     test.add_composition(postgres);
 
     test.run(|ops| async move {
         let container = ops.handle("postgres");
-        let conn_string = format!("postgres://postgres:postgres@{}:{}", container.ip(), 5432);
+        let conn_string = format!("postgres://postgres:password@{}:{}", container.ip(), 5432);
         let pgconn = PgConnection::establish(&conn_string);
 
         assert!(
