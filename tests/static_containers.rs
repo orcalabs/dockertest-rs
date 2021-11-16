c3dc78b3c285f4f7008cef247c1fbfd127c1c589
Vegard Sandengen <vegardsandengen@gmail.com>
2021-11-16 23:51:16 +0100

    test: add failing test for external static containers

--- tests/static_containers.rs
+++ tests/static_containers.rs
@@ -1,5 +1,13 @@
-use dockertest::{Composition, DockerTest, Image, PullPolicy, Source, StaticManagementPolicy};
+//! Integration test for static dockertest containers,
+
+use dockertest::{
+    utils::{connect_with_local_or_tls_defaults, generate_random_string},
+    Composition, DockerTest, Image, PullPolicy, Source, StaticManagementPolicy,
+};
+
+use bollard::container::{Config, CreateContainerOptions, StartContainerOptions};
 use lazy_static::lazy_static;
+
 use std::sync::{Arc, Mutex};
 
 lazy_static! {
@@ -24,6 +32,57 @@ fn test_static_containers_runs() {
     test.run(|_ops| async {
         assert!(true);
     });
+}
+
+#[tokio::test]
+async fn test_external_static_container_handle_resolves_correctly_mixed_with_others() {
+    let source = Source::DockerHub(PullPolicy::IfNotPresent);
+    let mut test = DockerTest::new().with_default_source(source);
+    let repo = "hello-world";
+
+    let d1_name = "frantic";
+    let d2_name = "marvelous";
+    let s1_name = format!("extravagant-{}", generate_random_string(20));
+
+    let d1 = Composition::with_repository(repo).with_container_name(d1_name);
+    let d2 = Composition::with_repository(repo).with_container_name(d2_name);
+    let mut s1 = Composition::with_repository(repo).with_container_name(&s1_name);
+
+    s1.static_container(StaticManagementPolicy::External);
+
+    test.add_composition(d1);
+    test.add_composition(s1);
+    test.add_composition(d2);
+
+    // Run the external container
+    let client = connect_with_local_or_tls_defaults().expect("connect to docker engine");
+    let config = Config::<String> {
+        image: Some(
+            client
+                .inspect_image(&format!("{}:{}", "dockertest-rs/hello", "latest"))
+                .await
+                .map(|res| res.id)
+                .expect("should get image id"),
+        ),
+        ..Default::default()
+    };
+    let options = Some(CreateContainerOptions { name: &s1_name });
+    let id = client
+        .create_container(options, config)
+        .await
+        .expect("create external container")
+        .id;
+    client
+        .start_container(&id, None::<StartContainerOptions<String>>)
+        .await
+        .expect("start external container");
+
+    test.run_async(|ops| async move {
+        assert!(ops.handle(&s1_name).name().contains(&s1_name));
+        assert!(ops.handle(d1_name).name().contains(d1_name));
+        assert!(ops.handle(d2_name).name().contains(d2_name));
+    })
+    .await;
 }
 
 #[test]
